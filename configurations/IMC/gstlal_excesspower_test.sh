2d65df338f87ba033d5d0a39239f7b6626dac1ca
Chris Pankow <chris.pankow@LIGO.ORG>
2014-04-04 14:41:03 -0400

    IMC config: tweak test script

--- configurations/IMC/gstlal_excesspower_test.sh
+++ configurations/IMC/gstlal_excesspower_test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 channel_name="IMC-F_OUT_DQ"
 sample_rate=4096
-init_file=something.ini
+init_file=config.txt
 frame_cache=something.cache
+start=1076669952
+end=1076670952
 
-gstlal_excesspower --data-source frames --channel-name L1=${channel_name} --sample-rate ${sample_rate} --initialization-file ${init_file} --verbose --clustering --frame-cache ${frame_cache} --verbose
+gstlal_excesspower --data-source frames --channel-name L1=${channel_name} --sample-rate ${sample_rate} --initialization-file ${init_file} --gps-start-time ${start} --gps-end-time ${end} --clustering --frame-cache ${frame_cache} --verbose
